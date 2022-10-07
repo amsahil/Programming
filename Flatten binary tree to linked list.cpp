02198443e2f69196ee2dbe63085504a61f8c8b68
amsahil <amsahilshah@gmail.com>
2022-10-07 23:59:39 +0530

    POTD-gfg

--- Flatten binary tree to linked list.cpp
+++ Flatten binary tree to linked list.cpp
@@ -1,3 +1,94 @@
+/* D10
+
+Flatten binary tree to linked list
+
+
+
+P.S:-Given the root of a binary tree, flatten the tree into a "linked list":
+The "linked list" should use the same Node class where the right child pointer points to the next
+node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree..
+
+Example 1:
+Input :
+          1
+        /   \
+       2     5
+      / \     \
+     3   4     6
+Output :
+1 2 3 4 5 6
+Explanation:
+After flattening, the tree looks
+like this
+    1
+     \
+      2
+       \
+        3
+         \
+          4
+           \
+            5
+             \
+              6
+Here, left of each node points
+to NULL and right contains the
+next node in preorder.The inorder
+traversal of this flattened tree
+is 1 2 3 4 5 6..
+
+Example 2:
+Input :
+        1
+       / \
+      3   4
+         /
+        2
+         \
+          5
+Output :
+1 3 4 2 5
+Explanation :
+After flattening, the tree looks
+like this
+     1
+      \
+       3
+        \
+         4
+          \
+           2
+            \
+             5
+Here, left of each node points
+to NULL and right contains the
+next node in preorder.The inorder
+traversal of this flattened tree
+is 1 3 4 2 5.
+
+
+
+Constraints:
+1<=n<=10^5
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+*/
+
+
+
+//--------------------------------------------------------------SOLUTION--------------------------------------------------
+
+
+
+
+
+
+
+
+
+
 //{ Driver Code Starts
 //Initial Template for C++
 
