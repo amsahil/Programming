d61da05c9baa58f16b0f85087b5f540a343a1fc9
amsahil <amsahilshah@gmail.com>
2022-10-09 23:50:45 +0530

    POTD-gfg

--- Help Ishaan.cpp
+++ Help Ishaan.cpp
@@ -1,3 +1,34 @@
+/* D12
+
+Help Ishaan
+
+P.S:- Ishaan has been given a task by his teacher. He needs to find the Nth term of a series.
+    His teacher gives him some examples to help him out (Refer examples below).
+    He is a bit weak in pattern searching so to help him his teacher told him that the Nth term is related to prime numbers.
+    The Nth term is the difference of N and the closest prime number to N. Help him find the Nth term for a given N.
+
+
+Example 1:
+Input: N = 10
+Output: 1
+Explanation: Closest prime to 10 is 11.
+
+Example 2:
+Input: N = 7
+Output: 0
+Explanation: Closest prime to 7 is 7.
+
+Constraints:
+1 <= N <= 100000
+
+Expected Time Complexity: O(N* √ N)
+Expected Space Complexity: O(1)
+
+*/
+
+
+//--------------------------------------------------------------SOLUTION-----------------------------------------------
+
 //{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
